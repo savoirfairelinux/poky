32e34ec60f0ee84c9a3be0f985510092a6f27145
Jean-Marie LEMETAYER <jean-marie.lemetayer@savoirfairelinux.com>
2019-10-16 11:48:57 +0200

    devtool/standard.py: update the append file for the npm recipes
    When creating a recipe using devtool, a workspace is created to store
    the new recipe, the recipe source and some append files. These append
    files are used by devtool to build the recipe using externalsrc (to use
    the source which are in the workspace). They can also have some
    additional actions according to the class of the recipe.
    
    This commit updates the append file for the npm recipes. The
    devtool / externalsrc files are removed in the npm build directory
    instead of the install directory.
    
    Signed-off-by: Jean-Marie LEMETAYER <jean-marie.lemetayer@savoirfairelinux.com>

--- scripts/lib/devtool/standard.py
+++ scripts/lib/devtool/standard.py
@@ -262,14 +262,11 @@ def add(args, config, basepath, workspace):
                 f.write('}\n')
 
             if bb.data.inherits_class('npm', rd):
-                f.write('do_install_append() {\n')
-                f.write('    # Remove files added to source dir by devtool/externalsrc\n')
-                f.write('    rm -f ${NPM_INSTALLDIR}/singletask.lock\n')
-                f.write('    rm -rf ${NPM_INSTALLDIR}/.git\n')
-                f.write('    rm -rf ${NPM_INSTALLDIR}/oe-local-files\n')
-                f.write('    for symlink in ${EXTERNALSRC_SYMLINKS} ; do\n')
-                f.write('        rm -f ${NPM_INSTALLDIR}/${symlink%%:*}\n')
-                f.write('    done\n')
+                f.write('do_compile_append() {\n')
+                f.write('    rm -rf ${B}/lib/node_modules/*/.git\n')
+                f.write('    rm -rf ${B}/lib/node_modules/@*/*/.git\n')
+                f.write('    rm -f ${B}/lib/node_modules/*/singletask.lock\n')
+                f.write('    rm -f ${B}/lib/node_modules/@*/*/singletask.lock\n')
                 f.write('}\n')
 
         # Check if the new layer provides recipes whose priorities have been
